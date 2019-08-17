b591e2fd0c99cffb4fddb02cfb50ba53c55516b3
pavan <pavan@LAPTOP-FMR2F5AL>
2019-08-17 10:02:55 +0530

    first commit to git

---
+++ com.gmail.login/src/test/java/com/gmail/login/Test1.java
@@ -0,0 +1,13 @@
+package com.gmail.login;
+
+import org.testng.annotations.Test;
+
+public class Test1
+{
+	@Test
+	public void helloWorld()throws Exception
+	{
+		System.out.println("Hello World");
+	}
+
+}
